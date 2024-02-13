49c1016f8e63b37712b40ad7ed4ddb03e9fe7cfe
ShayanAKhun <littelmater391@gmail.com>
2024-02-14 01:12:33 +0500

    first kotlin app

---
+++ app/src/main/java/com/example/newkotlinbasics/bacis.kt
@@ -0,0 +1,18 @@
+package com.example.newkotlinbasics
+
+fun main() {
+
+//        val myStr = "hello world"
+//        var newStr = myStr[0]
+//        var lastStr = myStr[myStr.length - 1]
+//    print("First Charater $newStr")
+
+//
+//    var x = 100
+//     while ( x >= 0){
+//         println("$x")
+//         x -=1
+//     }
+
+
+}
