2b8228074af1e0e7e92d9f8b3244c487f197058a
ShayanAKhun <littelmater391@gmail.com>
2024-02-25 21:33:05 +0500

    fixed interfaces

--- app/src/main/java/com/example/newkotlinbasics/bacis.kt
+++ app/src/main/java/com/example/newkotlinbasics/bacis.kt
@@ -1,33 +1,55 @@
 package com.example.newkotlinbasics
  // base class super class or parent class
-open class Car (val name: String, val brand : String) {
+
+interface Driveable {
+     val maxSpeed: Double
+        fun drive() : String
+        fun brake(){
+            println("The driveable is braking ")
+        }
+}
+
+
+open class Car (override val maxSpeed:Double, val name: String, val brand : String) : Driveable {
     //imhertited from vehical parent class
    open var range : Double = 0.0
      fun extendRange (amount : Double) {
          if(amount > 0)
               range += amount
      }
-   open  fun drive (distance : Double) {
+     override fun drive(): String {
+        return "Driving the interface drive"
+    }
+     open fun drive (distance : Double) {
          println("Drove for $distance KM")
      }
 
-     fun drive () {
-         println("Drove for $range KM on electricity")
-     }
+
 }
-class ElectriceCar(name: String, brand: String, batteryLife: Double) : Car(name, brand) {
+class ElectriceCar(maxSpeed: Double,name: String, brand: String, batteryLife: Double) : Car(maxSpeed ,name, brand) {
     override var range = batteryLife * 6
 
     override fun drive (distance: Double) {
         println("drove for $distance on electricty")
     }
+    override fun drive() : String {
+        return "Drove for  $range KM on electricity"
+    }
+
+    override fun brake() {
+        super.brake()
+    }
 }
 fun main () {
-    var myCar = Car("911", "Porsche")
-    var myEcar = ElectriceCar("S-Model", "Tesla", 85.0)
+    var myCar = Car(200.0,"A3", "Porsche")
+    var myEcar = ElectriceCar(240.0,"S-Model", "Tesla", 85.0)
 
-    myEcar.extendRange(300.0)
-    myCar.drive(200.0)
-    myEcar.drive(200.0)
-    myEcar.drive()
+
+    myEcar.brake()
+    myCar.brake()
+
+//    myEcar.extendRange(300.0)
+//    myCar.drive(200.0)
+//    myEcar.drive(200.0)
+//    myEcar.drive()
 }
