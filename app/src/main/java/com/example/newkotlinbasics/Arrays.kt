3c7ffd1014aa47acaa1e6da5e04eae419df4e7f7
ShayanAKhun <littelmater391@gmail.com>
2024-02-28 21:09:40 +0500

    arrays in Kotlin

---
+++ app/src/main/java/com/example/newkotlinbasics/Arrays.kt
@@ -0,0 +1,9 @@
+package com.example.newkotlinbasics
+
+fun main () {
+    val number1 : IntArray = intArrayOf(1,2,3,4,5,6)
+
+    val number2 = intArrayOf(1,2,3,4,5,6)
+
+    val number3 = arrayOf(1,2,3,4,5,6)
+}
