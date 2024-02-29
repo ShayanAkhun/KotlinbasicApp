f1e2a1f3452a1f390ef90bc16d31e5c06986cfda
ShayanAKhun <littelmater391@gmail.com>
2024-02-29 23:47:44 +0500

    lists

---
+++ app/src/main/java/com/example/newkotlinbasics/Lists.kt
@@ -0,0 +1,9 @@
+package com.example.newkotlinbasics
+
+fun main () {
+
+    val months = listOf("january","February", "March")
+    val anyType = listOf(1,2,3,true,false, "String")
+    print(anyType)
+    print(months[1])
+}
